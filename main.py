be3455c0ee62c91dee4f8bebdb5ed308c9117433
Leonardo Corti <leonardo.filippo@ymail.com>
2025-11-05 19:04:21 +0100

    structure

--- main.py
+++ main.py
@@ -1,5 +1,53 @@
+from fastapi import FastAPI
+import uvicorn
+
+app = FastAPI(
+    title="simple timelapse server",
+    description="simple timelapse server",
+    version="0.0.1",
+)
+
+# Global state variable
+is_running = False
+save_path = ""
+
+
+@app.post("/start")
+def start():
+    global is_running
+    if is_running:
+        print("error is already running")
+        return {}
+    is_running = True
+    print("starting")
+    # creazione cartella
+    global save_path
+    return {}
+
+
+@app.post("/photo")
+def photo():
+    global is_running
+    if is_running:
+        print("photo")
+        # fai foto e salvala
+    return {}
+
+
+@app.post("/end")
+def end():
+    global is_running
+    if not is_running:
+        print("Erorr was not running")
+        return {}
+    is_running = False
+    print("ending")
+    # creazione timelpase
+    return {}
+
+
 def main():
-    print("Hello from timelapse-simple-server!")
+    uvicorn.run("main:app", host="0.0.0.0", port=8085, reload=True)
 
 
 if __name__ == "__main__":
